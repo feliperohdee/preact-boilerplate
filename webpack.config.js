6bbe015a4263ec28a5d4b3ddcb1476c18f346308
Felipe Rohde <feliperohdee@gmail.com>
2018-03-14 23:57:33 -0300

    added support for cur files

--- webpack.config.js
+++ webpack.config.js
@@ -144,7 +144,7 @@ module.exports = (env = {}) => {
 				test: /\.(xml|html|txt|md)$/,
 				loader: 'raw-loader'
 			}, {
-				test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif|mp4|mov|ogg|webm)(\?.*)?$/i,
+				test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif|mp4|mov|ogg|webm|cur)(\?.*)?$/i,
 				loader: prod ? 'file-loader' : 'url-loader',
 				options: prod ? {
 					outputPath: 'assets'
